3d5ffab2604917ce66a3869dfb65928c61a7b523
tomasjacobsen <post@tomasjacobsen.com>
2020-12-03 22:06:11 +0100

    text

--- pages/hva-er-cafekafaen.vue
+++ pages/hva-er-cafekafaen.vue
@@ -8,17 +8,15 @@
  <b-row class="justify-content-md-center">
  <b-col cols="12" md="8">
 
- <p>En høstdag nede på Fisketorget hadde Davidsen, Ringe og Pallesen hatt et par halvlitere med gode idèer i. Det var trendy med pop-ups, og hvorfor kunne ikke vår gjeng lage et konsept? Vi skulle gjøre det som et stunt og en engangsgreie for familie og venner. Overskuddet skulle gå til en veldedig organisasjon, og vi måtte gjøre det på vår måte.</p>
- <p>Vi bestemte oss for å lage vår type mat, ha lave skuldre, spille høy musikk og ha god stemning. Cafeen skulle også ha som mål å arrangeres et sted det ikke var tilrettelagt for tradisjonell restaurantdrift. Hele greia ble mer eller mindre tatt på hælen, og uten videre ambisjoner.</p>
- <p>Det viser seg at folk synes det er ganske festlig med god mat og boksøl. Vi solgte ut det vi hadde av billetter og ble på fortalt at dette måtte vi gjøre igjen. Og sånn har det blitt. I dag er vi en Veldedig stiftelse som har som mål å gjennomføre et event i året. Vi er tretten personer i styret med ulike ansvarsområdet.</p>
- <p>Mantraen “vi har ikke mer kjekt enn vi gjør det til selv” er gjennomgående. Vi har vært 80 meter over bakken, i frisørsalonger og gamle verkstedhaller for å nevne noe.Vi har samlet inn penger til mennesker som trenger det mer enn oss, og det skal vi fortsette med. Vi er stolte av merkevaren vår, og at så mange sponsorer synes det er kjekt å bli assosiert med denne. </p>
+ <p>En høstdag nede på Fisketorget hadde Davidsen, Ringe og Pallesen hatt et par halvlitere med gode idèer i. Det var trendy med pop-ups, og hvorfor kunne ikke vår gjeng lage et konsept? Vi skulle  gjøre det som et stunt og en engangsgreie for familie og venner. Overskuddet skulle gå til en veldedig organisasjon, og vi måtte gjøre det på vår måte.</p>
+ <p>Vi bestemte oss for å lage god mat, ha lave skuldre, spille høy musikk og ha kjempestemning. Cafeen skulle også ha som mål å arrangeres et sted det ikke var tilrettelagt for tradisjonell restaurantdrift. Hele greia ble mer eller mindre tatt på hælen, og uten videre ambisjoner. Det viser seg at folk synes det er ganske festlig med god mat og boksøl. Vi solgte ut det vi hadde av billetter og ble på fortalt at dette måtte vi gjøre igjen. I dag er vi en Veldedig stiftelse som har som mål å gjennomføre et event i året. Vi er tretten personer i styret med ulike ansvarsområdet. Mantraen “vi har ikke mer gøy enn vi gjør det til selv” er gjennomgående. Vi har vært i 80 meter høye tårn,  frisørsalonger og gamle verkstedhaller for å nevne noe.Vi har samlet inn penger til mennesker som trenger det mer enn oss, og det skal vi fortsette med. Vi er stolte av merkevaren vår, og at så mange sponsorer synes det er kjekt å bli assosiert med denne. </p>
 
 
 
  <hr>
  <h2>Ofte stilte spørsmål</h2>
  <p><strong>Hvordan får man tak i billetter?</strong><br>
- Billettsalg blir annonsert på vår <a href="https://www.facebook.com/Cafè-KaFaen-1242056495939083/" target="_blank">facebookside</a> og administrert derfra. I fjor solgte vi ut på nitti sekunder, og det er “first come, first serve” som gjelder. Det er mulig vi inngår et samarbeid med ekstern billettformidler i fremtiden, men dette vil også bli annonsert.</p>
+ Billettsalg blir annonsert på vår <a href="https://www.facebook.com/Cafè-KaFaen-1242056495939083/" target="_blank">facebookside</a> facebookside og administrert derfra. I fjor solgte vi ut på nitti sekunder, og det er “first come, first serve” som gjelder. Det er mulig vi inngår et samarbeid med ekstern billettformidler i fremtiden, men dette vil også bli annonsert.</p>
  <p><strong>Hvor mye koster det?</strong><br>
  Det har variert, men de siste gangene har prisen pr billett ligget på rundt to tusen kroner. Drikke er ikke inkludert i denne prisen og må medbringes.</p>
  <p><strong>Hva går overskuddet til?</strong><br>
